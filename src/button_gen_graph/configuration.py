d582db82a2bab6c51e2f20487053c454f33e5d30
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-03-14 16:26:26 +0300

    fix import prompt in button gen

--- src/button_gen_graph/configuration.py
+++ src/button_gen_graph/configuration.py
@@ -7,7 +7,7 @@ from typing import Annotated, Any, Literal, Optional, Type, TypeVar
 
 from langchain_core.runnables import RunnableConfig, ensure_config
 
-from retrieval_graph import prompts
+from button_gen_graph import prompts
 
 T = TypeVar("T", bound="ButtonGenConfiguration")
 
