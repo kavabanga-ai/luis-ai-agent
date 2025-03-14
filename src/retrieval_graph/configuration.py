86f9ca3c7b1edf1df204c7d3ffb6698cbba442eb
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-03-14 16:30:55 +0300

    fix config ref sum butn conf

--- src/retrieval_graph/configuration.py
+++ src/retrieval_graph/configuration.py
@@ -9,7 +9,7 @@ from langchain_core.runnables import RunnableConfig, ensure_config
 
 from retrieval_graph import prompts
 
-T = TypeVar("T", bound="ButtonGenConfiguration")
+T = TypeVar("T", bound="IndexConfiguration")
 
 
 @dataclass(kw_only=True)
@@ -153,84 +153,3 @@ class Configuration(IndexConfiguration):
         default=prompts.REFERENCE_SYSTEM_PROMPT,
         metadata={"description": "The system prompt used for give reference to the reponse"},
     )
-
-
-@dataclass(kw_only=True)
-class SummarizationConfiguration:
-    """Configuration class for the summarization agent."""
-
-    user_id: str = field(metadata={"description": "Unique identifier for the user."})
-
-    summary_system_prompt: str = field(
-        default=prompts.SUMMARY_SYSTEM_PROMPT,
-        metadata={"description": "The system prompt used for summarization."},
-    )
-
-    summary_system_prompt_utm: str = field(
-        default=prompts.SUMMARY_SYSTEM_PROMPT_UTM,
-        metadata={"description": "The system prompt used for summarization. with keywords"},
-    )
-
-    summary_model: Annotated[str, {"__template_metadata__": {"kind": "llm"}}] = field(
-        default="openai/gpt-4o",
-        metadata={
-            "description": "The language model used for summarization. Should be in the form: provider/model-name."
-        },
-    )
-
-    @classmethod
-    def from_runnable_config(
-            cls: Type[T], config: Optional[RunnableConfig] = None
-    ) -> T:
-        """Create a SummarizationConfiguration instance from a RunnableConfig object.
-
-        Args:
-            cls (Type[T]): The class itself.
-            config (Optional[RunnableConfig]): The configuration object to use.
-
-        Returns:
-            T: An instance of SummarizationConfiguration with the specified configuration.
-        """
-        config = ensure_config(config)
-        configurable = config.get("configurable") or {}
-        _fields = {f.name for f in fields(cls) if f.init}
-        return cls(**{k: v for k, v in configurable.items() if k in _fields})
-
-
-@dataclass(kw_only=True)
-class ButtonGenConfiguration:
-    """Configuration class for the button generation agent."""
-
-    user_id: str = field(metadata={"description": "Unique identifier for the user."})
-
-    button_gen_system_prompt: str = field(
-        default=prompts.BUTTON_GEN_SYSTEM_PROMPT,
-        metadata={"description": "The system prompt used for button generation."},
-    )
-
-    button_gen_model: Annotated[str, {"__template_metadata__": {"kind": "llm"}}] = field(
-        default="openai/gpt-4",
-        metadata={
-            "description": "The language model used for button generation. Should be in the form: provider/model-name."
-        },
-    )
-
-    button_max_character: Optional[int] = field(
-        default=25,
-        metadata={"description": "Max button characters."},
-    )
-
-    @classmethod
-    def from_runnable_config(cls: Type[T], config: Optional[RunnableConfig] = None) -> T:
-        """Create a ButtonGenConfiguration instance from a RunnableConfig object.
-
-        Args:
-            config (Optional[RunnableConfig]): The configuration object to use.
-
-        Returns:
-            T: An instance of ButtonGenConfiguration with the specified configuration.
-        """
-        config = ensure_config(config)
-        configurable = config.get("configurable") or {}
-        _fields = {f.name for f in fields(cls) if f.init}
-        return cls(**{k: v for k, v in configurable.items() if k in _fields})
