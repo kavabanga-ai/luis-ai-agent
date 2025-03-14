0a4be22bd05b8980a748aa34ed7a641d213dc211
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-03-14 16:26:31 +0300

    fix prompt in button gen

--- src/button_gen_graph/prompts.py
+++ src/button_gen_graph/prompts.py
@@ -1,51 +1,5 @@
 """Default prompts."""
 
-RESPONSE_SYSTEM_PROMPT = """You are a helpful AI assistant. Answer the user's questions based on the retrieved documents.
-
-{retrieved_docs}
-
-System time: {system_time}"""
-
-RESPONSE_SYSTEM_PROMPT_WITH_LINK = """You are a helpful AI assistant. Answer the user's questions based on the retrieved documents.
-
-{retrieved_docs}
-
-System time: {system_time}"""
-
-QUERY_SYSTEM_PROMPT = """Generate search queries to retrieve documents that may help answer the user's question. Previously, you made the following queries:
-    
-<previous_queries/>
-{queries}
-</previous_queries>
-
-System time: {system_time}"""
-
-# New prompt for the summarization graph
-SUMMARY_SYSTEM_PROMPT = """You have to create structured and attractive article summaries. You always respond in 
-Russian and work with the provided texts to extract the main idea and important details. When you receive an article, 
-you create one key sentence reflecting the essence of the material (up to 100 characters). Then you form a 
-two-paragraph summary, broken down into bullet points with brief explanations, emphasizing key aspects of the 
-content. You aim for structure, avoiding unnecessary details."""
-
-SUMMARY_SYSTEM_PROMPT_UTM = """You specialize in creating short and engaging summaries of articles designed to be 
-displayed on a widget when a user visits a website. When you receive an article, you read through the content to 
-create a summary that captures the essence and context of the material.
-
-You will also be presented with keywords. You should select from these those keywords contained in the article and 
-incorporate them into your summary in a creative and meaningful way, enhancing its relevance and appeal.
-
-The main goal is to bring the main idea and main benefit to the reader of the article in one sentence in the form of 
-a promotional teaser that they will want to click on, up to 100 characters long.
-
-After that, you should offer a few brief bullet points that answer the question, “What services or assistance can I, 
-as an AI, offer to someone reading this article to save them time to research on their own?” These bullet points 
-should be phrased in an attractive and useful way, reflecting the main ideas and context of the article without 
-duplicating similar services.
-
-You respond only in Russian. You should not offer similar services or outside recommendations."""
-
-# prompts.py
-
 BUTTON_GEN_SYSTEM_PROMPT = """You are an assistant that, given an article, generates three buttons and then questions 
 and answers for those buttons.
 
@@ -62,68 +16,6 @@ For each button, generate a question that the user might ask, and an answer base
 Ensure the output strictly adheres to the provided format.
 
 Do not include any extra text or explanations."""
-
-APPOINTMENT_SYSTEM_PROMPT = """You always give doctors address with link for appointment"""
-APPOINTMENT_SYSTEM_PROMPT_CONDITION = """Analyze the user's input to determine if it indicates an intention to
-             book an appointment with a doctor. Respond with 'yes' if the input clearly reflects
-              this intention (e.g., 'I need a doctor appointment,' 'Can I book a doctor?' or 'Are
-               there doctors available?'), and 'no' if it does not."""
-
-MEDICAL_TEST_SYSTEM_PROMPT = """ """
-
-MEDICAL_TEST_SYSTEM_PROMPT_CONDITION = """ """
-
-CHECK_LINK_SYSTEM_PROMPT = """Carefully analyze the user question with the ai message if there is any to determine if they are 
-requesting a link or resources. or similar to that then Respond with 'yes' if they are, and 'no' if they are not."""
-
-CHECK_SAME_URL_SYSTEM_PROMPT = """Your task is to rewrite the provided text by replacing the original URL ({
-page_url}) with one of the URLs from the given source list ({source_urls}). While doing so, ensure that the overall 
-meaning, structure, and tone of the text remain unchanged. Do not add extra information or otherwise alter the core 
-message. The final result should read smoothly, as though the substituted URL was part of the original content all 
-along."""
-
-CHECK_SAME_DUPLICATE_URL_SYSTEM_PROMPT = """Your task is to rewrite the provided text by removing duplicate URLs, 
-whether they appear in the content or the source list ({source_urls}). Replace one of the duplicates in the content 
-with the most suitable unique URL from the source list that best fits the context. Ensure that the replacement 
-maintains the original meaning, structure, and tone of the text. Avoid adding extra information, modifying the 
-content unnecessarily, or altering the core message. The final text should flow naturally and remain clear, 
-as though the chosen URL was part of the original content."""
-
-REFERENCE_SYSTEM_PROMPT = """You are a helpful assistant tasked with providing structured and informative responses based on user-provided text. Follow these guidelines to ensure consistency and quality in your responses:
-
-1. **Analyze the Input:** Carefully read the user’s input text to identify if there are existing links or references provided.
-
-2. **Provide Links Based on Input:**
-   - If the input contains links or references, use them directly in your response.
-   - If there are no links in the input, extract relevant links from `{source_urls}` to supplement your response.
-
-3. **Structured Responses:**
-   - Always start your response with: `Информация собрана на основании статей:` followed by the list of links, e.g., `[1](url), [2](url)`.
-   - If applicable, conclude your response with an engaging question like: `Хотите узнать еще больше интересной и полезной информации по этой теме?`
-
-4. **Tone and Style:**
-   - Maintain a professional yet friendly tone.
-   - Use clear and concise language.
-
-5. **Dynamic URL Replacement:**
-   - Replace `{source_urls}` with actual URLs before generating the response.
-   - If `{source_urls}` contains no URLs, exclude the sources section and provide a disclaimer: `Ксожалению, список источников пуст. Могу попытаться дополнить ваш запрос, если получу дополнительную информацию.`
-
-6. **Example Scenarios:**
-   - **Case 1: User text includes links**
-     - _User’s text:_ Вот некоторые симптомы... [Изменения печени](url)
-     - _Response:_ Информация собрана на основании статей: [1](url)
-     - Хотите узнать...
-   
-   - **Case 2: User text does not include links**
-     - _User’s text:_ Симптомы, указывающие на...
-     - _Response:_ Информация собрана на основании статей: [1](https://example1.com), [2](https://example2.com)
-     - Хотите узнать...
-
-7. **Fallback Plan:**
-   - If the provided `{source_urls}` do not contain relevant links, respond with a polite disclaimer: `Ксожалению, список источников пуст. Могу попытаться дополнить ваш запрос, если получу дополнительную информацию.`
-
- """
 
 CHECK_FOR_RETRIEVAL_SYSTEM_PROMPT = """Analyze the user's input to determine if it is a simple greeting (e.g., "hi,
 " "hello") or farewell Respond with 'yes' if it is, and 'no' if it is not."""
